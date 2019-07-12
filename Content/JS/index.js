ff0d410be38c2f2b820686c9cf551e77bda31157
NguyenVietHaUet <45221989+NguyenVietHaUet@users.noreply.github.com>
2019-07-12 08:48:55 +0700

    Add files via upload

---
+++ Content/JS/index.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    $(".submit").click(function () {
+        if (!$('.userName').val()) {
+            $('.spanUser').addClass('warning');
+            $('.userName').addClass('borderRed');
+        }
+        if (!$('.password').val()) {
+            $('.spanPass').addClass('warning');
+            $('.password').addClass('borderRed');
+        }
+    })
+    $('.userName').blur(function () {
+        var value = this.value;
+        if (!value) {
+            $('.spanUser').addClass('warning');
+            $('.userName').addClass('borderRed');
+        } else {
+            $('.spanUser').removeClass('warning');
+            $('.userName').removeClass('borderRed');
+        }
+    })
+    $('.password').blur(function () {
+        var value = this.value;
+        if (!value) {
+            $('.spanPass').addClass('warning');
+            $('.password').addClass('borderRed');
+        } else {
+            $('.spanPass').removeClass('warning');
+            $('.password').removeClass('borderRed');
+        }
+    })
+
+})
